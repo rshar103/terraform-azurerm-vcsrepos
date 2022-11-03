16d5d1ec221e4e8a87ac2cb3d0fc39dccd6972d1
rohit <rshar@hotmail.com>
2022-11-03 17:45:06 +0530

    version 1.0 of my tf msnifest

---
+++ resource_group.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "samprg" {
+name = "samrg"
+location = "East US"
+}
---
+++ versions.tf
@@ -0,0 +1,15 @@
+# Terraform Block
+terraform {
+    required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.0.0" 
+    }    
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+ features {}          
+}
+
